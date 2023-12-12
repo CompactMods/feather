e1577f7def029536c8bedd179b1120971ebd96e1
Ted Senft <code@robotgryphon.com>
2023-12-11 22:57:21 -0500

    Fix GraphValueEdge type params

--- src/main/java/dev/compactmods/feather/edge/GraphValueEdge.java
+++ src/main/java/dev/compactmods/feather/edge/GraphValueEdge.java
@@ -2,7 +2,7 @@ package dev.compactmods.feather.edge;
 
 import dev.compactmods.feather.node.Node;
 
-public interface GraphValueEdge<E, S, SN extends Node<S>, T, TN extends Node<T>> extends GraphEdge<SN, TN> {
+public interface GraphValueEdge<SN extends Node<?>, TN extends Node<?>, E> extends GraphEdge<SN, TN> {
 
     E value();
 }
